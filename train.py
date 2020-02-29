910c017c0ca56f09f5d0b9b027d064c3ac4f1f87
zichongli5 <zichongli5@gmail.com>
2020-02-29 18:15:51 +0800

    adam

--- train.py
+++ train.py
@@ -293,11 +293,11 @@ def main():
         n_head=opt.n_head,
         dropout=opt.dropout).to(device)
 
-    optimizer = ScheduledOptim(
-        optim.Adam(transformer.parameters(), betas=(0.9, 0.98), eps=1e-09),
-        opt.lr, opt.d_model, opt.n_warmup_steps)
+#    optimizer = ScheduledOptim(
+#        optim.Adam(transformer.parameters(), betas=(0.9, 0.98), eps=1e-09),
+#        opt.lr, opt.d_model, opt.n_warmup_steps)
     optimizer = optim.Adam(transformer.parameters(), betas=(0.9, 0.999), eps=opt.lr)
-#    train(transformer, training_data, validation_data, optimizer, device, opt)
+    train(transformer, training_data, validation_data, optimizer, device, opt)
 
 
 
